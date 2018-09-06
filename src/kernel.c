01d2c45dbb339d80bd5d9b13f29e3ba4585a309c
MaxXSoft <x@MaxXSoft.net>
2018-09-06 21:46:27 +0800

    (kernel) added some comments

--- src/kernel.c
+++ src/kernel.c
@@ -1,3 +1,14 @@
+/*
+    I am the bone of my processor.
+    Silicon is my body, and current is by blood.
+    I have created over a thousand bootloaders.
+    Unknown to Crash.
+    Nor known to Work.
+    Have withstood pain to initialize many stacks.
+    Yet, those pointers will never hold anything.
+    So I pray, Unlimited Boot Works.
+*/
+
 #include "include/uart.h"
 
 void KernelMain() {
