e407dd21b727d042f69cb6359668f23662634b94
MaxXSoft <x@MaxXSoft.net>
2018-09-06 19:34:38 +0800

    (soc) added some definitions

--- src/include/soc.h
+++ src/include/soc.h
@@ -1,6 +1,9 @@
 #ifndef UBW_SOC_H_
 #define UBW_SOC_H_
 
+// CPU
+#define CPU_CLOCK   60000000
+
 // GPIO
 #define GPIO_SWITCH (*(volatile unsigned *)0xbfd05000)
 #define GPIO_KEYPAD (*(volatile unsigned *)0xbfd05004)
@@ -30,9 +33,14 @@
 #define UART_LCR    (*(volatile unsigned *)0xbfd0100c)
 // modem control register
 #define UART_MCR    (*(volatile unsigned *)0xbfd01010)
+// line status register
+#define UART_LSR    (*(volatile unsigned *)0xbfd01014)
 // divisor latch register (LSB), enable when UART.LCR[7]
 #define UART_DLL    (*(volatile unsigned *)0xbfd01000)
 // divisor latch register (MSB), enable when UART.LCR[7]
 #define UART_DLM    (*(volatile unsigned *)0xbfd01004)
+#define UART_LCR_8B   3          // word length: 8 bits per character
+#define UART_LSR_DR   (1 << 0)   // data_ready
+#define UART_LSR_THRE (1 << 5)   // data_ready
 
 #endif // UBW_SOC_H_
