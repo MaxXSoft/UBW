4c6c52ac0dbf8ffdf7c61a4b2cccd08e5742027e
MaxXSoft <x@MaxXSoft.net>
2018-09-07 15:55:20 +0800

    (stdio) fixed some bugs and added function 'printf'

--- src/include/stdio.h
+++ src/include/stdio.h
@@ -7,5 +7,6 @@ int putchar(int ch);
 int getchar();
 int puts(const char *str);
 char *gets(char *str);
+int printf(const char *format, ...);
 
 #endif // UBW_INCLUDE_STDIO_H_
--- src/library/stdio.c
+++ src/library/stdio.c
@@ -1,5 +1,6 @@
 #include "../include/stdio.h"
 #include "../include/uart.h"
+#include "../include/type.h"
 
 int putchar(int ch) {
     PutByteUART(ch);
@@ -11,16 +12,106 @@ int getchar() {
 }
 
 int puts(const char *str) {
-    for (; !(*str); ++str) {
-        PutByteUART(*str);
-    }
+    for (; *str; ++str) PutByteUART(*str);
     PutByteUART('\n');
     return 0;
 }
 
 char *gets(char *str) {
-    for (char *ptr = str; !(*ptr) || *ptr == '\n'; ++ptr) {
-        *ptr = GetByteUART();
-    }
+    char *ptr = str - 1;
+    do {
+        *++ptr = GetByteUART();
+    } while (*ptr && *ptr != '\n');
+    *ptr = '\0';
     return str;
+}
+
+static void PrintInteger(int num, int base, int sign) {
+    // get value
+    unsigned int value;
+    if (sign && num < 0) {
+        value = -num;
+        PutByteUART('-');
+    }
+    else {
+        value = num;
+    }
+    // convert value
+    char buffer[64];
+    int i;
+    for (i = 0; value; ++i) {
+        buffer[i] = value % base;
+        value /= base;
+    }
+    // print value
+    for (--i; i >= 0; --i) {
+        char c = buffer[i];
+        PutByteUART(c <= 9 ? c + '0' : c - 10 + 'a');
+    }
+}
+
+int printf(const char *format, ...) {
+    __builtin_va_list ap;
+    __builtin_va_start(ap, format);
+    void **arg = ap;
+    for (int i = 0; format[i]; ++i) {
+        char c = format[i];
+        if (c == '%') {
+            // escape characters
+            switch (format[i + 1]) {
+                case 's': {
+                    const char *str = *arg;
+                    for (; *str; ++str) PutByteUART(*str);
+                    ++arg;
+                    ++i;
+                    break;
+                }
+                case 'c': {
+                    PutByteUART((uint8_t)*arg);
+                    ++arg;
+                    ++i;
+                    break;
+                }
+                case 'u': {
+                    PrintInteger((int)*arg, 10, 0);
+                    ++arg;
+                    ++i;
+                    break;
+                }
+                case 'd': {
+                    PrintInteger((int)*arg, 10, 1);
+                    ++arg;
+                    ++i;
+                    break;
+                }
+                case 'o': {
+                    PrintInteger((int)*arg, 8, 0);
+                    ++arg;
+                    ++i;
+                    break;
+                }
+                case 'b': {
+                    PrintInteger((int)*arg, 2, 0);
+                    ++arg;
+                    ++i;
+                    break;
+                }
+                case 'p': case 'x': {
+                    PrintInteger((int)*arg, 16, 0);
+                    ++arg;
+                    ++i;
+                    break;
+                }
+                case '%': {
+                    PutByteUART('%');
+                    ++i;
+                    break;
+                }
+            }
+        }
+        else {
+            PutByteUART(c);
+        }
+    }
+    return 0;
 }
