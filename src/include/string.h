6320d0c26168c233205d67d31670c53d631a2e06
MaxXSoft <x@MaxXSoft.net>
2018-09-07 11:32:31 +0800

    (string) updated library 'string' and added more functions

--- src/include/string.h
+++ src/include/string.h
@@ -12,4 +12,72 @@ int strncmp(const char *lhs, const char *rhs, size_t count);
 char *strchr(const char *str, int ch);
 char *strrchr(const char *str, int ch);
 
+#define RANGE(v, l, r) ((v) >= (l) && (v) <= (r))
+
+static inline int isalnum(int ch) {
+    return RANGE(ch, '0', '9') || RANGE(ch, 'a', 'z')
+            || RANGE(ch, 'A', 'Z');
+}
+
+static inline int isalpha(int ch) {
+    return RANGE(ch, 'a', 'z') || RANGE(ch, 'A', 'Z');
+}
+
+static inline int islower(int ch) {
+    return RANGE(ch, 'a', 'z');
+}
+
+static inline int isupper(int ch) {
+    return RANGE(ch, 'A', 'Z');
+}
+
+static inline int isdigit(int ch) {
+    return RANGE(ch, '0', '9');
+}
+
+static inline int isxdigit(int ch) {
+    return RANGE(ch, '0', '9') || RANGE(ch, 'a', 'f')
+            || RANGE(ch, 'A', 'F');
+}
+
+static inline int iscntrl(int ch) {
+    return RANGE(ch, '\0', '\x1F') || ch == '\x7F';
+}
+
+static inline int isgraph(int ch) {
+    return RANGE(ch, '!', '~');
+}
+
+static inline int isspace(int ch) {
+    return RANGE(ch, '\t', '\r') || ch == ' ';
+}
+
+static inline int isblank(int ch) {
+    return ch == '\t' || ch == ' ';
+}
+
+static inline int isprint(int ch) {
+    return RANGE(ch, ' ', '~');
+}
+
+static inline int ispunct(int ch) {
+    return RANGE(ch, '!', '/') || RANGE(ch, ':', '@')
+            || RANGE(ch, '[', '`') || RANGE(ch, '{', '~');
+}
+
+static inline int tolower(int ch) {
+    return RANGE(ch, 'A', 'Z') ? ch - 'A' + 'a' : ch;
+}
+
+static inline int toupper(int ch) {
+    return RANGE(ch, 'a', 'z') ? ch - 'a' + 'A' : ch;
+}
+
+#undef RANGE
+
+int atoi(const char *str);
+long atol(const char *str);
+long strtol(const char *str, char **str_end, int base);
+unsigned long strtoul(const char *str, char **str_end, int base);
+
 #endif // UBW_STRING_H_
--- src/library/string.c
+++ src/library/string.c
@@ -1,4 +1,5 @@
 #include "../include/string.h"
+#include "../include/limits.h"
 
 char *strcpy(char *dest, const char *src) {
     if (!dest) return NULL;
@@ -65,4 +66,147 @@ char *strrchr(const char *str, int ch) {
         if (*str == ch) ptr = str;
     }
     return ptr;
+}
+
+int atoi(const char *str) {
+    return (int)strtol(str, NULL, 10);
+}
+
+long atol(const char *str) {
+    return strtol(str, NULL, 10);
+}
+
+long strtol(const char *str, char **str_end, int base) {
+    char *ptr = str;
+    int ch, neg = 0;
+    // skip spaces
+    do {
+        ch = *ptr++;
+    } while (isspace(ch));
+    // handle the sign
+    if (ch == '-') {
+        neg = 1;
+        ch = *ptr++;
+    }
+    else if (ch == '+') {
+        ch = *ptr++;
+    }
+    // handle zero 
+    if ((base == 0 || base == 16) && ch == '0'
+            && (*ptr == 'x' || *ptr == 'X')) {
+        // skip "0x"
+        ch = ptr[1];
+        ptr += 2;
+        // hexadecimal string
+        base = 16;
+    }
+    // auto-detect the base
+    if (base == '0') {
+        base = ch == '0' ? 8 : 10;
+    }
+    /*
+     * Compute the cutoff value between legal numbers and illegal
+     * numbers.  That is the largest legal value, divided by the
+     * base.  An input number that is greater than this value, if
+     * followed by a legal input character, is too big.  One that
+     * is equal to this value may be valid or not; the limit
+     * between valid and invalid numbers is then based on the last
+     * digit.  For instance, if the range for longs is
+     * [-2147483648..2147483647] and the input base is 10,
+     * cutoff will be set to 214748364 and cutlim to either
+     * 7 (neg==0) or 8 (neg==1), meaning that if we have accumulated
+     * a value > 214748364, or equal but the next digit is > 7 (or 8),
+     * the number is too big, and we will return a range error.
+     *
+     * Set any if any `digits' consumed; make it negative to indicate
+     * overflow.
+     */
+    unsigned long cutoff = neg ? -(unsigned long)LONG_MIN : LONG_MAX;
+    int cutlim = cutoff % (unsigned long)base;
+    cutoff /= (unsigned long)base;
+    int acc, any;
+    for (acc = 0, any = 0; ; ch = *ptr++) {
+        if (isdigit(ch)) {
+            ch -= '0';
+        }
+        else if (isalpha(ch)) {
+            ch -= isupper(ch) ? 'A' - 10 : 'a' - 10;
+        }
+        else {
+            break;
+        }
+        if (ch >= base) break;
+        if (any < 0 || acc > cutoff || (acc == cutoff && ch > cutlim)) {
+            any = -1;
+        }
+        else {
+            any = 1;
+            acc *= base;
+            acc += ch;
+        }
+    }
+    if (any < 0) {
+        acc = neg ? LONG_MIN : LONG_MAX;
+    }
+    else if (neg) {
+        acc = -acc;
+    }
+    if (str_end) *str_end = (char *)(any ? ptr - 1 : str);
+    return acc;
+}
+
+unsigned long strtoul(const char *str, char **str_end, int base) {
+    // similar to the above
+    char *ptr = str;
+    int ch, neg = 0;
+    do {
+        ch = *ptr++;
+    } while (isspace(ch));
+    if (ch == '-') {
+        neg = 1;
+        ch = *ptr++;
+    }
+    else if (ch == '+') {
+        ch = *ptr++;
+    }
+    if ((base == 0 || base == 16) && ch == '0'
+            && (*ptr == 'x' || *ptr == 'X')) {
+        ch = ptr[1];
+        ptr += 2;
+        base = 16;
+    }
+    if (base == '0') {
+        base = ch == '0' ? 8 : 10;
+    }
+    unsigned long cutoff = (unsigned long)ULONG_MAX / (unsigned long)base;
+    int cutlim = (unsigned long)ULONG_MAX % (unsigned long)base;
+    int acc, any;
+    for (acc = 0, any = 0; ; ch = *ptr++) {
+        if (isdigit(ch)) {
+            ch -= '0';
+        }
+        else if (isalpha(ch)) {
+            ch -= isupper(ch) ? 'A' - 10 : 'a' - 10;
+        }
+        else {
+            break;
+        }
+        if (ch >= base) break;
+        if (any < 0 || acc > cutoff || (acc == cutoff && ch > cutlim)) {
+            any = -1;
+        }
+        else {
+            any = 1;
+            acc *= base;
+            acc += ch;
+        }
+    }
+    if (any < 0) {
+        acc = ULONG_MAX;
+    }
+    else if (neg) {
+        acc = -acc;
+    }
+    if (str_end) *str_end = (char *)(any ? ptr - 1 : str);
+    return acc;
 }
