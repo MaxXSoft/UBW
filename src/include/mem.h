0488c8f916899f4b3079adad6d706300f1cf3b64
MaxXSoft <x@MaxXSoft.net>
2018-09-08 21:34:50 +0800

    (mem) updated the memory map related definitions

--- src/include/mem.h
+++ src/include/mem.h
@@ -3,6 +3,47 @@
 
 #include "type.h"
 
+/*
+        Memory Map of UBW (kseg0)
+
+        +-------------------+
+  9FFF  |                   |
+        |    unallocated    |
+  9FE0  |                   |
+        +-------------------+
+  9FDF  |                   |
+        |    peripherals    |
+  9FD0  |                   |
+        +-------------------+
+  9FCF  |                   |
+        |        ROM        |
+        |  (.text & .data)  |
+  9FC0  |                   |
+        +-------------------+
+  9FBF  |                   |
+        |    unallocated    |
+  8600  |                   |
+        +-------------------+
+  85FF  |                   |
+        |     video mem     |
+  8500  |                   |
+        +-------------------+
+  84FF  |                   |
+        |       heap        |
+  8400  |                   |
+        +-------------------+
+  83FF  |                   |
+        |       stack       |
+  8300  |                   |
+        +-------------------+
+  82FF  |    global mem     |
+        +-------------------+
+  82FE  |                   |
+        |       free        |
+  8000  |                   |
+        +-------------------+
+*/
+
 #ifndef MEM_HEAP_BASE
 #define MEM_HEAP_BASE 0x84000000
 #endif
@@ -10,6 +51,12 @@
 #ifndef MEM_HEAP_SIZE
 #define MEM_HEAP_SIZE 0x100000
 #endif
+
+#ifndef MEM_GLOBAL_BASE
+#define MEM_GLOBAL_BASE 0x82FF0000
+#endif
+
+#define MEM_GLOBAL_SIZE 0x0000FFFF
 
 void *malloc(size_t length);
 void free(void *ptr);
